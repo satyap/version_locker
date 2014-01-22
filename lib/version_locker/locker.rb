b6732315b6b1fc810a53aed3206adedb871f6cc6
Scott Holden <sholden@truecar.com>
2014-01-22 13:20:50 -0800

    adding locker

---
+++ lib/version_locker/locker.rb
@@ -0,0 +1,60 @@
+module VersionLocker
+  class Locker
+    attr_reader :options
+    GEM_PATTERN = /^(?<indentation> *)gem +['"](?<name>.+?)['"](, ['"](?<version>(~> *)?[\d\.]+)['"])?(?<options>, [^$#]+)?.*$/
+
+    def initialize(options = {})
+      @options = options
+    end
+
+    def spermy?
+      options.fetch(:spermy) { true }
+    end
+
+    def gemfile_path
+      options.fetch(:gemfile_path) { 'Gemfile' }
+    end
+
+    def lock!
+      target.write new_gemfile
+    end
+
+    private
+
+    def target
+      options.fetch(:target) { File.open(gemfile_path, 'w') }
+    end
+
+    def old_gemfile_lines
+      source_text = options.fetch(:source) { File.read(gemfile_path) }
+      source_text.lines
+    end
+
+    def new_gemfile
+      old_gemfile_lines.map {|line| new_gemfile_line(line) }.join
+    end
+
+    def new_gemfile_line(line)
+      if match = GEM_PATTERN.match(line) and valid_options?(match[:options]) and version = gem_version(match)
+        lock_line(match, version)
+      else
+        line
+      end
+    end
+
+    def valid_options?(options)
+      options !~ /git|path/
+    end
+
+    def gem_version(match)
+      return unless spec = Gem.loaded_specs[match[:name]]
+      spec.version
+    end
+
+    def lock_line(match, version)
+      line = "#{match[:indentation]} gem \"#{match[:name]}\", \"#{'~>' if spermy?}#{version}\"#{match[:options]}\n"
+    end
+  end
+end
+
+
